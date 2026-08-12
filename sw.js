b8155137ca3d5a1e6eb0ba63b8634c111ab687b6
rjxkr075-daizyken <rjxkr075@yahoo.co.jp>
2026-08-12 15:29:14 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
-const CACHE="bp-hem7271t-v3";
+const CACHE="bp-hem7271t-v4";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([
+  self.clients.claim(),
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+])));
 self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
