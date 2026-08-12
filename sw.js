48da8be622393273e02c41e838be021a45e1f494
rjxkr075-daizyken <rjxkr075@yahoo.co.jp>
2026-08-12 15:04:16 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="bp-photo-v1";
+const CACHE="bp-hem7271t-v2";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
